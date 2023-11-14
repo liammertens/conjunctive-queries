1e91573ab911a34a9bd32977a5f2cb8aac99c987
Liam Mertens <liam.mertens02@gmail.com>
2023-11-14 10:14:28 +0100

    Modified GYO to terminate on cyclic queries

--- algos/GYO.ts
+++ algos/GYO.ts
@@ -14,42 +14,46 @@ Implementation of the Graham-Yu-Ozsoyoglu (GYO) algorithm to test for acyclicty 
         - if only 1, set this node as root
         - if more, connect the roots of the trees in arbitrary fashion to get a single tree (TODO:)
 */
-export function GYO(query: Query): JoinTree {
+export function GYO(query: Query): JoinTree | undefined {
     const hg = new Hypergraph(query);
     const tree = new JoinTree;
 
-    // TODO: add a guard to prevent infinite recursion when query would be cyclic 
+    // if a full pass is done over all edges and none is removed, set this flag to false to prevent inf. recursion
+    let pass_again = true;
     while (hg.edges.length > 0) {
-        for (const edge of hg.edges) { // when we add to this array, the for loop will continue looping
-            const e = [...edge]; // convert to an array => do this only once
-            const res: [boolean, Set<string>] = ear(e, hg.edges.filter(x => !eqSet(x, edge)), new Set<string>(), new Array);
-            if (res[0]) { // e is an ear
-                let earNode = new Node(edge);
-                const oldEarNode = tree.getNode(earNode.toString());
-                if (oldEarNode) { // if ear was previously added to tree (as a witness), use the Node object already added to the tree
-                    earNode = oldEarNode;
-                } else {
-                    tree.addNode(earNode);
-                }                
-
-                if (res[1].size > 0) { // the chosen ear has a witness
-                    const witnessNode = new Node(res[1]);
-                    const witness = tree.getNode(witnessNode.toString());
-                    if (witness) { // witness already exists in tree
-                        witness.addChild(earNode);
-                        earNode.parent = witness;
-                    } else { // add witness to tree
-                        witnessNode.addChild(earNode);
-                        earNode.parent = witnessNode;
-                        tree.addNode(witnessNode)
+        if (pass_again) {
+            pass_again = false;
+            for (const edge of hg.edges) { // when we add to this array, the for loop will continue looping
+                const e = [...edge]; // convert to an array
+                const other_edges = hg.edges.filter(x => !eqSet(x, edge))
+                const res: [boolean, Set<string>] = ear(e, other_edges, other_edges, new Set<string>(), new Array());
+                if (res[0]) { // e is an ear
+                    let earNode = new Node(edge);
+                    const oldEarNode = tree.getNode(earNode.toString());
+                    if (oldEarNode) { // if ear was previously added to tree (as a witness), use the Node object already added to the tree
+                        earNode = oldEarNode;
+                    } else {
+                        tree.addNode(earNode);
+                    }                
+    
+                    if (res[1].size > 0) { // the chosen ear has a witness
+                        const witnessNode = new Node(res[1]);
+                        const witness = tree.getNode(witnessNode.toString());
+                        if (witness) { // witness already exists in tree
+                            witness.addChild(earNode);
+                            earNode.parent = witness;
+                        } else { // add witness to tree
+                            witnessNode.addChild(earNode);
+                            earNode.parent = witnessNode;
+                            tree.addNode(witnessNode)
+                        }
                     }
+                    hg.removeEdge(edge);
+                    pass_again = true;                 
                 }
-                hg.removeEdge(edge);                 
-            } else { // e is not an ear => skip e for now and advance to next
-                hg.removeEdge(edge);
-                hg.edges.push(edge);
             }
-            console.log(hg.edges);
+        } else {
+            return undefined;
         }
     }
     tree.setRoot();
@@ -61,34 +65,42 @@ Determines recursively whether e is an ear given a set of possible witnesses.
 If e is not an ear, the returned array has a false flag at idx 0.
 Returns an array with the witness at index 1. If no witness (e is completely isolated), then idx 1 = empty set
 
-e should be an array so we can use the Array.shift procedure
+e should be an array so we can use the Array.shift procedure. edges is the array of edges in the hypergraph excl. e (initially = witnesses).
+w is the currently to be tested witness and lastE is the set of vertices of e to be tested after choosing w as witness.
 
 An edge e is an ear iff.:
     - all vertices belonging to e are exclusive to e, or...
     - There exists another edge w such that every vertex in e is either exclusive to e or also occuring in w:
         - set w to an edge if w = undefined && w is one of possible witnesses
-        - if w is already set, check if the vertex occurs in w:
+        - if w is already set, check if the vertex occurs in w (or is isolated):
             - if not, discard w from possible witnesses and track back
 */
-export function ear(e: Array<string>, witnesses: Array<Set<string>>, w: Set<string>, lastE: Array<string>): [boolean, Set<string>] {
+export function ear(e: Array<string>, edges: Array<Set<string>>, witnesses: Array<Set<string>>, w: Set<string>, lastE: Array<string>): [boolean, Set<string>] {
     if (e.length > 0) {
+        /*
+        console.log('ear:', e)
+        console.log('witnesses:', witnesses)
+        console.log('lastE: ', lastE)
+        console.log('w:', w)
+        console.log('______________')
+        */
         const vertex = e[0];
-        if (witnesses.every((edge: Set<string>) => !edge.has(vertex))) { // vertex is isolated
+        if (edges.every((edge: Set<string>) => !edge.has(vertex))) { // vertex is isolated
             e.shift();
-            return ear(e, witnesses, w, lastE);
+            return ear(e, edges, witnesses, w, lastE);
         } else if (w.size > 0) { // vertex not isolated + witness was previously found
             if (w.has(vertex)) { // w is also a witness for this vertex
                 e.shift();
-                return ear(e, witnesses, w, lastE); // continue recursively
+                return ear(e, edges, witnesses, w, lastE); // continue recursively
             } else { // w is not a valid witness for vertex => can never be a valid witness for any vertex of e anymore!
                 const new_witnesses = witnesses.filter((el, idx, r) => !eqSet(el, w)); // remove w from possible witnesses
                 // backtrack using array so that a new witness cam be found starting from the point in time where the previous one was set
-                return ear(lastE, new_witnesses, new Set<string>(), new Array());
+                return ear(lastE, edges, new_witnesses, new Set<string>(), new Array());
             }
         } else if (witnesses.length == 0) { // vertex not isolated + no possible witnesses left => e is not an ear, return e at idx 0
             return [false, new Set<string>()];
         } else { // no w assigned yet
-            // recheck for isolation (loop over witnesses)
+            // search a witness w
             for (const edge of witnesses) {
                 if (edge.has(vertex)) {
                     w = edge;
@@ -96,7 +108,9 @@ export function ear(e: Array<string>, witnesses: Array<Set<string>>, w: Set<stri
                 }
             }
             if (w.size > 0) {
-                return ear(e, witnesses, w, e); // keep track of e after setting w
+                lastE = [...e] // copy e to avoid shifting lastE on e.shift()
+                e.shift();
+                return ear(e, edges, witnesses, w, lastE); // keep track of e after setting w
             } else { // no witness found after all => query must be cyclic
                 return [false, new Set<string>()]
             }
--- main.ts
+++ main.ts
@@ -2,9 +2,8 @@ import { DataType, Dictionary, Type, makeData } from 'apache-arrow';
 import { Relation } from './DB/relation';
 import { Query } from './DB/query';
 import { Atom } from './DB/atom';
-import { Variable } from './DB/variable';
 import { Hypergraph, eqSet } from './algos/hypergraph';
-import { Term } from './DB/term';
+import { ConstTerm, Term, VarTerm } from './DB/term';
 import { GYO, ear } from './algos/GYO';
 
 /*
@@ -24,34 +23,69 @@ function main(): void {
     const locations = new Relation('data/locations.csv');
     const styles = new Relation('data/styles.csv');
 
-
     const ex_query = new Query(new Atom(null, []), 
-        [new Atom(beers, 
+            [new Atom(beers, 
+                [
+                    new VarTerm('beer_id'),
+                    new VarTerm('brew_id'),
+                    new VarTerm('beer'),
+                    new VarTerm('abv'),
+                    new VarTerm('ibu'),
+                    new VarTerm('ounces'),
+                    new VarTerm('style'),
+                    new VarTerm('style2')
+                ]),
+            new Atom(styles,
+                [
+                    new VarTerm('style_id'),
+                    new VarTerm('cat_id'),
+                    new VarTerm('style')
+                ]),
+            new Atom(categories,
+                [
+                    new VarTerm('cat_id'),
+                    new ConstTerm('Belgian and French Ale')
+                ])]);
+    
+    // exercises chpt.2 E5 b.)
+    // only test this query using GYO, do not compute as relations have wrong arity
+    
+    const ex_query_cyclic = new Query(new Atom(null, []),
+        [new Atom(beers,
             [
-            new Term(new Variable('beer_id')),
-            new Term(new Variable('brew_id')),
-            new Term(new Variable('beer')),
-            new Term(new Variable('abv')),
-            new Term(new Variable('ibu')),
-            new Term(new Variable('ounces')),
-            new Term(new Variable('style')),
-            new Term(new Variable('style2'))
+                new VarTerm('x'),
+                new VarTerm('z')
             ]),
-        new Atom(styles,
+        new Atom(beers,
             [
-            new Term(new Variable('style_id')),
-            new Term(new Variable('cat_id')),
-            new Term(new Variable('style'))
+                new VarTerm('x'),
+                new VarTerm('y'),
+                new VarTerm('n'),
+                new VarTerm('w'),
+                new VarTerm('r')
             ]),
-        new Atom(categories,
+        new Atom(beers,
             [
-            new Term(new Variable('cat_id')),
-            new Term('Belgian and French Ale')
-            ])]);
-       
+                new VarTerm('r'),
+                new VarTerm('w')
+            ]),
+        new Atom(beers,
+            [
+                new VarTerm('z'),
+                new VarTerm('v')
+            ]),
+        new Atom(beers,
+            [
+                new VarTerm('v'),
+                new VarTerm('n')
+            ])])
+    
+    //const c = GYO(ex_query_cyclic);
+    //console.log(c?.nodes[0]); // (x, z) is taken as ear => incorrect
+
     const t = GYO(ex_query);
 
-    console.log(t.nodes[0].parent);
+    console.log(t?.nodes[0].parent);
 
 }
 
