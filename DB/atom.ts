ab25c07e6c4a946e3140fa3b3ecfc5da745393da
Liam Mertens <liam.mertens02@gmail.com>
2023-11-14 14:23:16 +0100

    modified tree to associate nodes with queries

--- DB/atom.ts
+++ DB/atom.ts
@@ -1,11 +1,20 @@
 import { Relation } from "./relation";
-import { Term } from "./term";
+import { Term, isVar } from "./term";
 
 export class Atom {
     relation: Relation | null;
     terms: Array<Term>;
+    variables: Set<string>;
     constructor(relation: Relation | null, terms: Array<Term>) {
         this.relation = relation;
         this.terms = terms;
+        // keep track of all variables
+        // the set variables is re-used during hypergraph creation, GYO (and thus in the join tree)
+        this.variables = new Set<string>;
+        for(const t of this.terms) {
+            if (isVar(t)) {
+                this.variables.add(t.val)
+            }
+        }
     }
 }
